dcc66235f576a8a38b385929992acbe855a17fed
Matheus Evaldt <evaldtmatheus@gmail.com>
2020-12-09 01:27:49 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,204 @@
+// Variables.
+const content = document.querySelector('.content');
+
+let clubs = [
+    {
+        name: 'Atlético Mineiro',
+        fullName: 'Clube Atlético Mineiro',
+        state: 'Minas Gerais',
+        supporters: '5',
+        titles: {
+            titlesClubWorldCup: '0',
+            titlesBrazilianChampionship: '1',
+            titlesBrazilianCup: '1',
+            titlesConmebolLibertadores: '1',
+            titlesConmebolSudamericana: '0',
+            titlesConmebolRecopa: '1',
+            titlesStateChampionship: '45'
+        }
+    },
+    {
+        name: 'Botafogo',
+        fullName: 'Botafogo de Futebol e Regatas',
+        state: 'Rio de Janeiro',
+        supporters: '2.8',
+        titles: {
+            titlesClubWorldCup: '0',
+            titlesBrazilianChampionship: '2',
+            titlesBrazilianCup: '0',
+            titlesConmebolLibertadores: '0',
+            titlesConmebolSudamericana: '0',
+            titlesConmebolRecopa: '0',
+            titlesStateChampionship: '21',
+        }
+    },
+    {
+        name: 'Corinthians',
+        fullName: 'Sport Club Corinthians Paulista',
+        state: 'São Paulo',
+        supporters: '28.3',
+        titles: {
+            titlesClubWorldCup: '2',
+            titlesBrazilianChampionship: '7',
+            titlesBrazilianCup: '3',
+            titlesConmebolLibertadores: '1',
+            titlesConmebolSudamericana: '0',
+            titlesConmebolRecopa: '1',
+            titlesStateChampionship: '30'
+        }
+    },
+    {
+        name: 'Cruzeiro',
+        fullName: 'Cruzeiro Esporte Clube',
+        state: 'Minas Gerais',
+        supporters: '7.2',
+        titles: {
+            titlesClubWorldCup: '0',
+            titlesBrazilianChampionship: '4',
+            titlesBrazilianCup: '6',
+            titlesConmebolLibertadores: '2',
+            titlesConmebolSudamericana: '0',
+            titlesConmebolRecopa: '1',
+            titlesStateChampionship: '36'
+        }
+    },
+    {
+        name: 'Flamengo',
+        fullName: 'Clube de Regatas do Flamengo',
+        state: 'Rio de Janeiro',
+        supporters: '37,9',
+        titles: {
+            titlesClubWorldCup: '1',
+            titlesBrazilianChampionship: '6',
+            titlesBrazilianCup: '3',
+            titlesConmebolLibertadores: '2',
+            titlesConmebolSudamericana: '0',
+            titlesConmebolRecopa: '1',
+            titlesStateChampionship: '35'
+        }
+    },
+    {
+        name: 'Fluminense',
+        fullName: 'Fluminense Football Club',
+        state: 'Rio de Janeiro',
+        supporters: '2.9',
+        titles: {
+            titlesClubWorldCup: '0',
+            titlesBrazilianChampionship: '4',
+            titlesBrazilianCup: '1',
+            titlesConmebolLibertadores: '0',
+            titlesConmebolSudamericana: '0',
+            titlesConmebolRecopa: '0',
+            titlesStateChampionship: '31'
+        }
+    },
+    {
+        name: 'Grêmio',
+        fullName: 'Grêmio Foot-Ball Porto Alegrense',
+        state: 'Rio Grande do Sul',
+        supporters: '8',
+        titles: {
+            titlesClubWorldCup: '1',
+            titlesBrazilianChampionship: '2',
+            titlesBrazilianCup: '5',
+            titlesConmebolLibertadores: '3',
+            titlesConmebolSudamericana: '0',
+            titlesConmebolRecopa: '2',
+            titlesStateChampionship: '38'
+        }
+    },
+    {
+        name: 'Internacional',
+        fullName: 'Sport Club Internacional',
+        state: 'Rio Grande do Sul',
+        supporters: '6.3',
+        titles: {
+            titlesClubWorldCup: '1',
+            titlesBrazilianChampionship: '3',
+            titlesBrazilianCup: '1',
+            titlesConmebolLibertadores: '2',
+            titlesConmebolSudamericana: '1',
+            titlesConmebolRecopa: '2',
+            titlesStateChampionship: '45'
+        }
+    },
+    {
+        name: 'Palmeiras',
+        fullName: 'Sociedade Esportiva Palmeiras',
+        state: 'São Paulo',
+        supporters: '13.1',
+        titles: {
+            titlesClubWorldCup: '0',
+            titlesBrazilianChampionship: '10',
+            titlesBrazilianCup: '3',
+            titlesConmebolLibertadores: '1',
+            titlesConmebolSudamericana: '0',
+            titlesConmebolRecopa: '0',
+            titlesStateChampionship: '24'
+        }
+    },
+    {
+        name: 'Santos',
+        fullName: 'Santos Futebol Clube',
+        state: 'São Paulo',
+        supporters: '5.5',
+        titles: {
+            titlesClubWorldCup: '2',
+            titlesBrazilianChampionship: '8',
+            titlesBrazilianCup: '1',
+            titlesConmebolLibertadores: '3',
+            titlesConmebolSudamericana: '0',
+            titlesConmebolRecopa: '1',
+            titlesStateChampionship: '22'
+        }
+    },
+    {
+        name: 'São Paulo',
+        fullName: 'São Paulo Futebol Clube',
+        state: 'São Paulo',
+        supporters: '17.1',
+        titles: {
+            titlesClubWorldCup: '3',
+            titlesBrazilianChampionship: '6',
+            titlesBrazilianCup: '0',
+            titlesConmebolLibertadores: '3',
+            titlesConmebolSudamericana: '1',
+            titlesConmebolRecopa: '2',
+            titlesStateChampionship: '21'
+        }
+    },
+    {
+        name: 'Vasco',
+        fullName: 'Clube de Regatas Vasco da Gama',
+        state: 'Rio de Janeiro',
+        supporters: '9.6',
+        titles: {
+            titlesClubWorldCup: '0',
+            titlesBrazilianChampionship: '4',
+            titlesBrazilianCup: '1',
+            titlesConmebolLibertadores: '1',
+            titlesConmebolSudamericana: '0',
+            titlesConmebolRecopa: '0',
+            titlesStateChampionship: '24'
+        }
+    }
+];
+
+function test() {
+    clubs.map(club => {
+        const div = document.createElement('div');
+        const name = document.createElement('p');
+        const fullName = document.createElement('p');
+        const state = document.createElement('p');
+        div.className = 'club';
+        name.innerHTML = club.name;
+        fullName.innerHTML = `Full name: ${club.fullName}`;
+        state.innerHTML = club.state;
+        div.appendChild(name);
+        div.appendChild(fullName);
+        div.appendChild(state);
+        content.appendChild(div);
+    });
+}
+
+test();
